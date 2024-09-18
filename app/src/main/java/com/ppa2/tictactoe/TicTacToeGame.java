8daf98e46ebcea9ec64bbea2b12a6241c4f17d59
Nighthawk500 <jafm0108@gmail.com>
2024-09-18 16:10:56 -0400

    Win / draw logic

--- app/src/main/java/com/ppa2/tictactoe/TicTacToeGame.java
+++ app/src/main/java/com/ppa2/tictactoe/TicTacToeGame.java
@@ -32,23 +32,23 @@ public class TicTacToeGame extends AppCompatActivity {
         }
     }
 
-    //Check for a win
-    public char gameWinner() {
-        // Check rows
+    // Check for a win
+    public boolean gameWinnerCheck() {
+        // Check for win by rows
         for (int i = 0; i < GRID_SIZE; i++) {
             if (mGameGrid[i][0] == currentPlayer && mGameGrid[i][1] == currentPlayer && mGameGrid[i][2] == currentPlayer) {
                 return true;
             }
         }
 
-        // Check columns win
+        // Check for win by columns
         for (int i = 0; i < GRID_SIZE; i++) {
             if (mGameGrid[0][i] == currentPlayer && mGameGrid[1][i] == currentPlayer && mGameGrid[2][i] == currentPlayer) {
                 return true;
             }
         }
 
-        // Check diagonals
+        // Check for win by diagonals
         if (mGameGrid[0][0] == currentPlayer && mGameGrid[1][1] == currentPlayer && mGameGrid[2][2] == currentPlayer) {
             return true;
         }
@@ -56,5 +56,20 @@ public class TicTacToeGame extends AppCompatActivity {
         if (mGameGrid[0][2] == currentPlayer && mGameGrid[1][1] == currentPlayer && mGameGrid[2][0] == currentPlayer) {
             return true;
         }
+        return false;
+    }
+
+    // Check for a draw
+    public boolean isDraw() {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                // If there is a spot remaining, game is not yet a draw
+                if (mGameGrid[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        // Game is a draw if all spots are filled
+        return true;
     }
 }
