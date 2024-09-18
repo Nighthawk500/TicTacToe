22b22179352984a76dc5b62917ccd682b743b3b1
Nighthawk500 <jafm0108@gmail.com>
2024-09-18 15:06:13 -0400

    + quit button to UI

--- app/src/main/java/com/ppa2/tictactoe/GameActivity.java
+++ app/src/main/java/com/ppa2/tictactoe/GameActivity.java
@@ -10,4 +10,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
+    public String getState() {
+        StringBuilder boardString = new StringBuilder();
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                char value = mGameGrid[row][col] ? 'X' : 'O';
+                boardString.append(value);
+            }
+        }
+    return boardString.toString();
+    }
+
+
 }
