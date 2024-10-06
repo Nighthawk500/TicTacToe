0822234e51c9b2ec264f331871b8633d446fa8d9
ferrisandrew <163781511+ferrisandrew@users.noreply.github.com>
2024-10-06 14:01:55 -0400

    fixed wrong player on new game

--- app/src/main/java/com/ppa2/tictactoe/MainActivity.java
+++ app/src/main/java/com/ppa2/tictactoe/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
             Button gridButton = (Button) tictactoegrid.getChildAt(buttonIndex);
             gridButton.setText("");
             gridButton.setEnabled(true);
+            playerIndicator.setText(String.valueOf(mGame.getCurrentPlayer() == 'X' ? 'O' : 'X'));
         }
     }
 
